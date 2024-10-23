54a74fc7faac1b5e9a8cdd15b61774fb134c3ac3
aiduJ <142336299+aiduJ@users.noreply.github.com>
2024-10-22 19:20:51 -0500

    created a html and script file

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let admin;
+let name = "Aiden";
+admin = name;
+alert(admin)
+
+alert(`Hello ${1+2}! because it is right`);
+
+[1, 2].forEach(alert);
+
+let age = prompt('What is your name?', 'John Doe');
+
+let result = confirm(`Are you ${name}?`)
+
+alert(result)
